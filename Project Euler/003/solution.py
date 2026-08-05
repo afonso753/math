2eee403cff21b65c6a361622e80b9124ae03a269
afonso753 <afonso.pereira.vilhena@gmail.com>
2026-08-05 11:49:37 +0100

    .

--- Project Euler/003/solution.py
+++ Project Euler/003/solution.py
@@ -1,11 +1,12 @@
+import math
+
 def isPrime(n):
-    i=2
-    prime = True
-    while(i<n and prime == True):
-        if(n%i==0):
-            prime = False
-        else: i+=1
-    return prime
+    if (n < 2):
+        return False
+    for i in range (2, int(math.sqrt(n))+1):
+        if (n%i == 0):
+            return False
+    return True
             
 
 def largestPrime(n):
